ff6b7bd1bdf38261c0e9cf03398a81d4e2eacecb
Adham Elarabawy <adhamelarabawy@gmail.com>
2020-05-05 01:54:48 -0700

    finished contact section

--- main.js
+++ main.js
@@ -6,10 +6,8 @@ $(window).on("load",function() {
       var objectBottom = $(this).offset().top + $(this).outerHeight();
 
       /* If the element is completely within bounds of the window, fade it in */
-      if (objectBottom < windowBottom-10) { //object comes into view (scrolling down)
+      if (objectBottom < windowBottom+5) { //object comes into view (scrolling down)
         if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
-      } else { //object goes out of view (scrolling up)
-        if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
       }
     });
   }).scroll(); //invoke scroll-handler on page-load
