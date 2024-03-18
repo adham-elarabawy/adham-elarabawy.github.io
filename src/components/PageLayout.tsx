0093e6297f474dce571985ccda208d52c822c7ba
Adham <adham@harvey.ai>
2024-03-18 13:17:38 -0700

    update

---
+++ src/components/PageLayout.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ScrollArea, Transition } from '@mantine/core';
+import { Header } from "../components/Header/Header";
+import { Footer } from "../components/Footer/Footer";
+import { useLocation } from "@reach/router"; // Correct import of useLocation
+
+export const PageLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const location = useLocation(); // Use the useLocation hook to get the current location object
+
+  return (
+    <Transition transition="fade" duration={150} timingFunction="ease-out" mounted={true}>
+      {(styles) => (
+        <div style={styles}>
+          <Header location={location} /> // Pass the location object to the Header component
+            <div className="mx-auto mb-12 max-w-5xl">{children}</div>
+          <Footer />
+        </div>
+      )}
+    </Transition>
+  );
+};
