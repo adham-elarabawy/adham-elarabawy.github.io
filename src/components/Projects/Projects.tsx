bc001a08869bed0a7399628da7e0946f69538d3d
Adham <adham@harvey.ai>
2024-03-18 11:32:42 -0700

    responsive grid instead of single column

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -2,21 +2,28 @@ import React from "react";
 import { PageLayout } from "../../components/PageLayout";
 import { graphql } from "gatsby";
 import { ProjectCard } from "../../components/ProjectCard/ProjectCard";
-import { Center } from "@mantine/core";
+import { Center, SimpleGrid } from "@mantine/core";
 
 export default function Projects({ data }) {
   const projects = data.allMdx.nodes.sort((a, b) => a.frontmatter.sort_id - b.frontmatter.sort_id);
 
 
   return (
-      <Center style={{ marginTop: '20px' }}>
-        <div> 
+        <Center style={{ marginTop: '20px' }}>
+            <SimpleGrid cols={{ base: 1, sm: 2}} spacing="sm" verticalSpacing="sm">
+                {projects.map(project => (
+                    <div key={project.id}>
+                    <ProjectCard project={project.frontmatter} />
+                    </div>
+                ))}
+            </SimpleGrid>
+        {/* <div> 
           {projects.map(project => (
             <div key={project.id}>
               <ProjectCard project={project.frontmatter} />
             </div>
           ))}
-        </div>
+        </div> */}
       </Center>
   );
 }
